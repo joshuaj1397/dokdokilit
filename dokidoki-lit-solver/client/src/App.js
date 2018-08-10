fc751c982a0698775a7842c072f79f63b601152d
joshuaj1397 <joshuaj1397@gmail.com>
2018-08-10 12:32:19 -0500

    Recovered earlier version of React App

--- dokidoki-lit-solver/client/src/App.js
+++ dokidoki-lit-solver/client/src/App.js
@@ -1,59 +1,114 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './css/bootstrap.min.css'
 
 // Functional Components
-function Search(props) {
+function Graph(props) {
+  if (props.data.edges && props.data.vertices) {
+    const vertices = props.data.vertices;
+    const edgeMatrix = props.data.edges;
 
+    const verticesList = vertices.map((vertice) => {
+      return <Vertice data={vertice} ref={React.createRef()}/>
+    });
+
+    var edges = [];
+    for (var i = 0; i < edgeMatrix.length; i++) {
+      var startVertice = verticesList[i];
+      for (var j = 0; j < edgeMatrix[i].length; j++) {
+        var endVertice = verticesList[j];
+        edges.push(<Edge startVertice={startVertice.ref.current} endVertice={endVertice.ref.current}></Edge>);
+      }
+    }
+
+    return (
+      <div>
+        <div className='row'>
+          {verticesList}
+        </div>
+          {edges}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
-function Node(props) {
-  return (
-    <button className="Node">
-      "Placeholder"
-    </button>
-  );
+class Vertice extends Component {
+  constructor(props) {
+    super(props);
+    this.div = null;
+  }
+
+  render() {
+    return (
+      <div className='col-sm-3'>
+        <div className='vertice card w-75' ref={ele => this.div = ele}>
+          <div className='card-body'>
+            <div className='card-title'>{this.props.data.name}</div>
+            <div className='card-text'>{this.props.data.desc}</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-function Edge(props) {
-  return (
-    <svg class="Edge">
-      <line x1="0" y1="0" x2="200" y2="200" />
-    </svg>
-  )
+class Edge extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    if (this.props.startVertice && this.props.endVertice) {
+      console.log(this.props.startVertice.ref)
+      var tail = this.props.startVertice.div.getBoundingClientRect();
+      var head = this.props.endVertice.div.getBoundingClientRect();
+      return (
+        <svg className='Edge'>
+          <line x1={tail.x} y1={tail.y} x2={head.x} y2={head.y} />
+        </svg>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 class App extends Component {
   state = {
-    response: ''
+    response: {}
   };
 
   componentDidMount() {
-    this.get()
+    this.callApi('graph')
       .then(res => this.setState({
         response: res.express
       }))
       .catch(err => console.log(err));
   }
 
-  getNode = async() => {
-    const response = await fetch('/api/getNode');
+  callApi = async(route) => {
+    const response = await fetch('/api/' + route);
     const body = await response.json();
-
     if (response.status !== 200) throw Error(body.message);
 
     return body;
   }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to the DokiDoki Lit Solver</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.response}
-        </p>
+      <div className='App container-fluid'>
+        <div className='row'>
+          <div className='col'>
+            <h1 className='App-header'>Welcome to the Doki Doki Lit Solver!</h1>
+          </div>
+        </div>
+        <Graph data={this.state.response}/>
       </div>
     );
   }
